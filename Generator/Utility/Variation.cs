cc758777b717e5da17ae28e1da374ea36fc5604e
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-28 19:51:12 +0300

    `feat:` implement `Variation` class

---
+++ Generator/Utility/Variation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generator.Utility
+{
+    /// <summary>
+    /// Representing a class of computing the variation.
+    /// </summary>
+    public static class Variation
+    {
+        public static Double Compute(List<UInt64> values)
+        {
+            Double result = 0.0;
+
+            Double expect = Expectation.Compute(values);
+
+            foreach (UInt64 value in values)
+            {
+                result += (value - expect) * (value - expect);
+            }
+
+            return result / values.Count;
+        }
+    }
+}
