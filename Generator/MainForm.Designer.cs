664c12db0dfde6bda6d81bb71bd86ca46e54a556
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-28 19:49:37 +0300

    `refactor:` implement designer for `MainForm`

---
+++ Generator/MainForm.Designer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Generator
+{
+    public partial class MainForm
+    {
+        private IContainer components = null;
+
+        private void InitializeComponent()
+        {
+            this.components = new Container();
+
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(800, 600);
+            this.Text = "Random Generator";
+        }
+
+        protected override void Dispose(Boolean disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
--- Generator/MainForm.cs
+++ Generator/MainForm.cs
@@ -1,31 +1,12 @@
-using System;
-using System.ComponentModel;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace Generator
 {
     public partial class MainForm : Form
     {
-        private IContainer? components = null;
-
         public MainForm()
         {
-            this.components = new Container();
-
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(800, 600);
-            this.Text = "Random Generator";
-        }
-
-        protected override void Dispose(Boolean disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-
-            base.Dispose(disposing);
+            InitializeComponent();
         }
     }
 }
