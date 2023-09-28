1dc3f3040e20b717b0b2c31b58da8210a9875b3f
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-28 22:30:41 +0300

    `feat:` define `IRandomable` interface

---
+++ Generator/Algorithms/IRandomable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generator.Algorithms
+{
+    /// <summary>
+    /// Defines a method to generate next <see cref="UInt64"/>.
+    /// </summary>
+    public interface IRandomable
+    {
+        UInt64 Next();
+    }
+}
