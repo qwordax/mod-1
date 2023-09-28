697a78f5b9fd86e034afef463d597c88ac0fc032
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-28 19:50:37 +0300

    `feat:` implement `Expectation` class

---
+++ Generator/Utility/Expectation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generator.Utility
+{
+    /// <summary>
+    /// Represents a class of computing the expectation.
+    /// </summary>
+    public static class Expectation
+    {
+        public static Double Compute(List<UInt64> values)
+        {
+            Double result = 0.0;
+
+            foreach (UInt64 value in values)
+            {
+                result += value;
+            }
+
+            return result / values.Count;
+        }
+    }
+}
