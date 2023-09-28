d4549078922256f0cbb3065374a68885c676a4b5
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-28 22:34:05 +0300

    `feat:` implement `BuiltIn` class

---
+++ Generator/Algorithms/BuiltIn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generator.Algorithms
+{
+    /// <summary>
+    /// Built-in <see cref="Random"/> class.
+    /// </summary>
+    public class BuiltIn : IRandomable
+    {
+        private Random random = new Random();
+
+        public BuiltIn() { }
+
+        public UInt64 Next()
+        {
+            Byte[] bytes = new Byte[64];
+            random.NextBytes(bytes);
+
+            return BitConverter.ToUInt64(bytes);
+        }
+    }
+}
