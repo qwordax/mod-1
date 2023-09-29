b707738ea6e15c447c37e2c379367c4a61c8522f
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-29 10:34:21 +0300

    `feat:` add dummy `parameterBox`

--- Generator/MainForm.Designer.cs
+++ Generator/MainForm.Designer.cs
@@ -5,17 +5,28 @@ namespace Generator
 {
     public partial class MainForm
     {
+        private GroupBox parameterBox;
         private GroupBox generatorBox;
 
         private void InitializeComponent()
         {
+            parameterBox = new GroupBox();
             generatorBox = new GroupBox();
 
+            parameterBox.SuspendLayout();
             generatorBox.SuspendLayout();
 
             SuspendLayout();
 
-            generatorBox.Location = new Point(5, 5);
+            parameterBox.Location = new Point(5, 5);
+            parameterBox.Margin = new Padding(0, 0, 0, 0);
+            parameterBox.Name = "parameterBox";
+            parameterBox.Size = new Size(300, 100);
+            parameterBox.TabIndex = 0;
+            parameterBox.TabStop = false;
+            parameterBox.Text = "Parameters";
+
+            generatorBox.Location = new Point(5, 110);
             generatorBox.Margin = new Padding(0, 0, 0, 0);
             generatorBox.Name = "generatorBox";
             generatorBox.Size = new Size(300, 100);
@@ -30,8 +41,10 @@ namespace Generator
             MinimizeBox = false;
             Text = "Random Generator";
 
+            Controls.Add(parameterBox);
             Controls.Add(generatorBox);
 
+            parameterBox.ResumeLayout(true);
             generatorBox.ResumeLayout(true);
 
             ResumeLayout(false);
