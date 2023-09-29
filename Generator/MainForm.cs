f9fd567e9e53be3caa9932c9a9fb0447bb7a8196
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-29 08:21:43 +0300

    `docs:` add `MainForm` summary

--- Generator/MainForm.cs
+++ Generator/MainForm.cs
@@ -2,6 +2,9 @@ using System.Windows.Forms;
 
 namespace Generator
 {
+    /// <summary>
+    /// Represents the main window of the application.
+    /// </summary>
     public partial class MainForm : Form
     {
         public MainForm()
