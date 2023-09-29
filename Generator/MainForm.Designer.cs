ce8276a271ed86e041a2e57a9a0935d7f7a46f9a
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-29 10:15:08 +0300

    `feat:` update designer

--- Generator/MainForm.Designer.cs
+++ Generator/MainForm.Designer.cs
@@ -1,31 +1,19 @@
-﻿using System;
-using System.ComponentModel;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace Generator
 {
     public partial class MainForm
     {
-        private IContainer components = null;
-
         private void InitializeComponent()
         {
-            this.components = new Container();
+            SuspendLayout();
 
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(800, 600);
-            this.Text = "Random Generator";
-        }
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 600);
+            Text = "Random Generator";
 
-        protected override void Dispose(Boolean disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-
-            base.Dispose(disposing);
+            ResumeLayout(false);
         }
     }
 }
