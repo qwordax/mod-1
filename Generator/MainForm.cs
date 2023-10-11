7c1cbf84aff71c41419475c5772ad0bae763bf69
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-11 15:11:19 +0300

    `refactor:` delete obsolete using

--- Generator/MainForm.cs
+++ Generator/MainForm.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
 
 using Generator.Algorithms;
 using Generator.Utilities;
